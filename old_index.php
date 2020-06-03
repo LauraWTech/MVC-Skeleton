09884d865159fb6fba89d0301f643b672432a6cd
HannahSargeant <Hannahsargeant16@gmail.com>
2020-06-03 10:29:39 +0100

    oldindex changes for mac

--- old_index.php
+++ old_index.php
@@ -1,26 +1,9 @@
 <!DOCTYPE html>
-    <?php /*session_start()*/;?>
+
 <html>
-  <!--  <head>
+    <head>
         <meta charset="UTF-8">
         <title>ADVNTR</title>
-    </head>-->
-
-   <?php/*
-    require_once('connection.php');
-
-    if (isset($_GET['controller']) && isset($_GET['action'])) {
-        $controller = $_GET['controller'];
-        $action = $_GET['action'];
-    } else {
-        $controller = 'pages';
-        $action = 'home';
-    }
-
-    require_once('views/layout.php');*/
-    ?>
-
-    <head>
         <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
